5d3c7d993a0cd150ccaef9074dbb07e1128b8202
dstyazhkin <dstyazhkin@gmail.com>
2018-09-14 13:19:03 +0300

    updated

--- Repsly_api_parser.R
+++ Repsly_api_parser.R
@@ -2,14 +2,35 @@ library(httr)
 library(xml2)
 library(googlesheets)
 
+extracted_data_nrows <- 1
+
+load("last_id.RData")
+#last_id <- 0
+
+
+extracted_data_to_upload <- data.frame(matrix(ncol = 9, nrow = 0))
+colnames(extracted_data_to_upload) <-c('PhotoId',  
+                                       'ClientCode',
+                                       'ClientName',
+                                       'Note',  
+                                       'DateTime',
+                                       'PhotoURL',
+                                       'RepresentativeCode',
+                                       'RepresentativeName',
+                                       'VisitId')
+
+while(extracted_data_nrows > 0) 
+{
 query_params <- list(`contentType` =  "application/json")
-get_result <- GET(url = "https://api.repsly.com/v3/export/photos/2", 
+get_result <- GET(url = paste0("https://api.repsly.com/v3/export/photos/", last_id), 
                   authenticate(user = "8F509842-1964-4474-9650-12DCBCF2E002", 
                                 password = "1B39F561-7BAC-4AD4-9061-C7BA395D3011"),
                   query = query_params)
 xml_t <- content(get_result, as = "text")
 xml_r<- read_xml(xml_t)
 #xml_structure(xml_r)
+
+
 
 photo_id <- xml_text(
               xml_find_all(xml_r , "//Photos/Photo/PhotoID")
@@ -56,7 +77,21 @@ extracted_data <- data.frame(
   'VisitId' = visit_id
 )
 
+extracted_data_nrows <- nrow(extracted_data)
+if(extracted_data_nrows > 0)
+{
+  extracted_data_to_upload <- rbind(extracted_data)
+}
+last_id <- xml_text(
+  xml_find_all(xml_r , "//LastID"))
+}
+
+last_id <- max(as.numeric(as.character(extracted_data_to_upload$PhotoId)))
+save(last_id, file = "last_id.RData")
 
 #Работа с Google Sheet 
-write.csv(extracted_data, "repsly_api_R_extraction.csv", row.names = FALSE)
-repsly_api_R_extraction <- gs_upload("repsly_api_R_extraction.csv") #загрузка в Google Sheet
+#write.csv(extracted_data, "repsly_api_R_extraction.csv", row.names = FALSE)
+#repsly_api_R_extraction <- gs_upload("repsly_api_R_extraction.csv") #загрузка в Google Sheet
+repsly_data_sheet <- gs_title("repsly_api_R_extraction")
+gs_add_row(repsly_data_sheet, input = extracted_data_to_upload)
+
